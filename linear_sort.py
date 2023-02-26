86d0a8dcd48bfe4f60a8d774ae05a79467e15ffc
Kamaren McCord <KamarenMcCord@yahoo.com>
2023-02-26 15:07:10 -0600

    added expected variables to be used

--- linear_sort.py
+++ linear_sort.py
@@ -2,8 +2,20 @@
 
 import test_loader
 
+# main variables
 test_data = test_loader.get_input_size()
+integrity_skip = False
+time_taken = 0
 
+# integrity test skip
+""" should only be used for testing that may take an exessive amount of time 
+the integrity test would double the time the testing can take to finish which can be problematic on longer tests 
+like: O(!) then linear searching 100,000 bits!!"""
+integrity_skip=False
+if integrity_skip:
+    passed_integrity = True
+
+# get current time before running
 for firstIterator in test_data:
     for secondIterator in test_data:
         if firstIterator > secondIterator:
@@ -11,8 +23,12 @@ for firstIterator in test_data:
             secondIterator = firstIterator
             firstIterator = swapVar
 
-#test it
+# get time after running
+# get difference on times and store this number
 
-# print it
-print(test_data)
+#test the integrity of the sort using a linear search
+
+# print how long it took and note wheather or note the integrity test passed
+
+
 # closure or loopback
