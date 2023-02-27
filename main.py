648de1990f25cbc217d64e4629ec1f0a31049c61
Kamaren McCord <KamarenMcCord@yahoo.com>
2023-02-27 11:34:04 -0600

    added main loop, exit option, offloaded main program. Should add prompts and get the algorithm selectable

--- main.py
+++ main.py
@@ -3,41 +3,68 @@ import test_loader
 from buble_sort import bubble_sort
 
 # main variables
-test_data = test_loader.get_input_size()
-integrity_skip = False
-time_taken = 0
-start_time = 0
-end_time = 0
+loop_switch = True
 
-# integrity test skip
-"""
-should only be used for testing 
-this may take an exessive amount of time 
-the integrity test can double the time or worse 
-like: O(!) then linear searching (test) 100,000 entities!!
-"""
-integrity_skip=False
-if integrity_skip:
-    passed_integrity = True
+algorithm_options = [
+    "bubble_sort",
+]
 
-# get current time before running
-start_time = test_loader.check_time()
+exit_words = [
+    "quit", "exit", "end", "stop",
+]
 
-# Do a sort - may be a function call later
-bubble_sort(test_data)
+def exit_function():
+    #print text to screen
+    # pause for a second for the user to read
+    # exit program
+    exit()
 
+def execute_algo():
+    # Main vars
+    # integrity test skip
+    """
+    should only be used for testing 
+    this may take an exessive amount of time 
+    the integrity test can double the time or worse 
+    like: O(!) then linear searching (test) 100,000 entities!!
+    """
+    integrity_skip = False
+    start_time, end_time = 0, 0
+    # Get input size before mesuring the time
+    test_data = test_loader.get_input_size()
 
-# get time after running
-end_time = test_loader.check_time()
-# get difference on times and store this number
-time_taken = end_time - start_time
+    # time before the program ran
+    start_time = test_loader.check_time()
+    # run an algo
+    bubble_sort(test_data)
+    # get time after running
+    end_time = test_loader.check_time()
 
-#test the integrity of the sort
-if not integrity_skip:
-    test_passed = test_loader.integrity_check(test_data)
-    print(f'Integrity test passed: {test_passed}')
-    if test_passed == False:
-        print(test_data[:25])
+    #test the integrity of the sort
+    if not integrity_skip:
+        test_passed = test_loader.integrity_check(test_data)
+        print(f'Integrity test passed: {test_passed}')
+        if test_passed == False:
+            print(test_data[:25])
 
-# print how long it took to sort
-print(test_loader.time_calc(start_time, end_time, len(test_data)))
+    # print how long it took to sort
+    print(test_loader.time_calc(start_time, end_time, len(test_data)))
+
+def get_input():
+    print("enter something here:")
+    return input("> ")
+
+### TODO
+# print greeting to program and explain what does
+
+while loop_switch:
+    # print entry data / option info
+    # ask for input to stop loop
+    user_option = get_input()
+    # check input for false condition
+    if user_option in exit_words:
+        loop_switch = False
+        exit_function()
+    else:
+        # create end condition
+        execute_algo()
