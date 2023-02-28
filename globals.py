8a17d4ce2045a273c1a321f2ee130fb89bf2cc1d
Kamaren McCord <KamarenMcCord@yahoo.com>
2023-02-28 13:29:41 -0600

    working on front end to be adapted to mulitple prompts and be able to except input size. May rework the interation for the gui to take mulitple optionsas to keep DRY

--- globals.py
+++ globals.py
@@ -1,12 +1,14 @@
 from buble_sort import bubble_sort
 
 OPTIONS = {
-    "CLEAR_SPEED": 2,
+    "CLEAR_SPEED": 2,   # how fast the prompt stops before freshing, for longer holds use a input statement
+    "INTEGRITY_SKIP": False,  # should only be used for debuging; test runs in o(N)
 }
 
-ALGORITHM_OPTIONS = [
-    {"name": "Bubble Sort", "function": bubble_sort},
-]
+ALGORITHM_OPTIONS = [{
+    "name": "Bubble Sort", 
+    "function": bubble_sort
+},]
 
 SETTINGS_KEYWORDS = [
     "settings", "config", "options", "option", "setting", "configure",
@@ -15,3 +17,5 @@ SETTINGS_KEYWORDS = [
 EXIT_WORDS = [
     "quit", "exit", "end", "stop", "esc"
 ]
+
+EXIT_MESSAGE = """ This is the exit message """
